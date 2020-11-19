5fe29f2501e717dc006d267f65623cac95446bf4
Kamil Nować <kano@netcompany.com>
2020-11-19 16:07:10 +0100

    Create appcenter-pre-build.sh (#16)
    * Create appcenter-pre-build.sh
    
    * Update OAuthConf.cs
    
    * Remove scope
    
    * Update auth header sed

--- NDB.Covid19/NDB.Covid19/Configuration/OAuthConf.cs
+++ NDB.Covid19/NDB.Covid19/Configuration/OAuthConf.cs
@@ -3,7 +3,7 @@ namespace NDB.Covid19.Configuration
     public static class OAuthConf
     {
         public static string OAUTH2_CLIENT_ID = "INJECTED_IN_APP_CENTER_DURING_BUILD";
-        public static string OAUTH2_SCOPE = "INJECTED_IN_APP_CENTER_DURING_BUILD";
+        public static string OAUTH2_SCOPE = "openid smittestop";
         public static string OAUTH2_REDIRECT_URL = "INJECTED_IN_APP_CENTER_DURING_BUILD";
         public static string OAUTH2_AUTHORISE_URL = "INJECTED_IN_APP_CENTER_DURING_BUILD";
         public static string OAUTH2_ACCESSTOKEN_URL = "INJECTED_IN_APP_CENTER_DURING_BUILD";
