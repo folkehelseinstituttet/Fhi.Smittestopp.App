0f33323996e1681b5aabc00225529922dab08b2a
Kamil Nowac <kano@netcompany.com>
2020-11-20 13:22:21 +0100

    Update ResendNotificationTests.cs

--- NDB.Covid19/NDB.Covid19.Test/Tests/ExposureNotification/ResendNotificationTests.cs
+++ NDB.Covid19/NDB.Covid19.Test/Tests/ExposureNotification/ResendNotificationTests.cs
@@ -31,29 +31,34 @@ namespace NDB.Covid19.Test.Tests.ExposureNotification
 
         [Theory]
         // Today
-        [InlineData(0, -1, false)] // Before 9PM
-        [InlineData(0, 1, false)] // After 9PM
-        [InlineData(0, 0, false)] // Equal 9PM
+        [InlineData(0, -1, false)] // Before 9AM
+        [InlineData(0, 1, false)] // After 9AM
+        [InlineData(0, 0, false)] // Equal 9AM
         // Next day
-        [InlineData(1, -1, false)] // Before 9PM
-        [InlineData(1, 1, false)] // After 9PM
-        [InlineData(1, 0, false)] // Equal 9PM
+        [InlineData(1, -1, false)] // Before 9AM
+        [InlineData(1, 1, false)] // After 9AM
+        [InlineData(1, 0, false)] // Equal 9AM
         // After 48 hours
-        [InlineData(2, -1, false)] // Before 9PM
-        [InlineData(2, 1, true)] // After 9PM
-        [InlineData(2, 0, true)] // Equal 9PM
-        [InlineData(3, -1, false)] // Before 9PM
-        [InlineData(3, 1, true)] // After 9PM
-        [InlineData(3, 0, true)] // Equal 9PM
+        [InlineData(2, -1, false)] // Before 9AM
+        [InlineData(2, 1, true)] // After 9AM
+        [InlineData(2, 0, true)] // Equal 9AM
+        // After 48hours but odd day
+        [InlineData(3, -1, false)] // Before 9AM
+        [InlineData(3, 1, false)] // After 9AM
+        [InlineData(3, 0, false)] // Equal 9AM
+        // After 48h but even day
+        [InlineData(4, -1, false)] // Before 9AM
+        [InlineData(4, 1, true)] // After 9AM
+        [InlineData(4, 0, true)] // Equal 9AM
         // Older than 14 days
-        [InlineData(14, -1, false)] // Before 9PM
-        [InlineData(14, 1, false)] // After 9PM
-        [InlineData(14, 0, false)] // Equal 9PM
+        [InlineData(14, -1, false)] // Before 9AM
+        [InlineData(14, 1, false)] // After 9AM
+        [InlineData(14, 0, false)] // Equal 9AM
         public async void ShouldUpdateLastMessageDate(int daysOfTimeShift, int minutesOfTimeShift, bool shouldBeCalled)
         {
             ResetData();
 
-            SystemTime.SetDateTime(DateTime.Now.Date.AddHours(12).ToUniversalTime());
+            SystemTime.SetDateTime(DateTime.Now.Date.AddHours(8).ToUniversalTime());
             await ServiceLocator.Current.GetInstance<IMessagesManager>().SaveNewMessage(new MessageSQLiteModel()
             {
                 TimeStamp = SystemTime.Now(),
@@ -66,14 +71,28 @@ namespace NDB.Covid19.Test.Tests.ExposureNotification
 
             DateTime preFetchDateTime = MessageUtils.GetDateTimeFromSecureStorageForKey(SecureStorageKeys.LAST_SENT_NOTIFICATION_UTC_KEY, "");
             SystemTime.SetDateTime(DateTime.Now.Date.AddDays(daysOfTimeShift).AddHours(21).AddMinutes(minutesOfTimeShift).ToUniversalTime());
+            
+            int initialDay =
+                daysOfTimeShift > 1 ? daysOfTimeShift - 2 : 0; // To fasten up the execution time
+            // Simulates pulling on each day
+            for (int i = initialDay; i <= daysOfTimeShift; i++) {
 
-            try
-            {
-                await new FetchExposureKeysHelper().FetchExposureKeyBatchFilesFromServerAsync(null, CancellationToken.None);
-            }
-            catch
-            {
-                // ignore as ZipDownloader is not mocked in this test
+                if (i == daysOfTimeShift)
+                {
+                    LocalNotificationsManager.HasBeenCalled[NotificationsEnum.NewMessageReceived] = false;
+                }
+                preFetchDateTime = MessageUtils.GetDateTimeFromSecureStorageForKey(SecureStorageKeys.LAST_SENT_NOTIFICATION_UTC_KEY, "");
+                SystemTime.SetDateTime(DateTime.Now.Date.AddDays(i).AddHours(9).AddMinutes(minutesOfTimeShift).ToUniversalTime());
+
+                try
+                {
+                    await new FetchExposureKeysHelper().FetchExposureKeyBatchFilesFromServerAsync(null,
+                        CancellationToken.None);
+                }
+                catch
+                {
+                    // ignore as ZipDownloader is not mocked in this test
+                }
             }
 
             Assert.Equal(shouldBeCalled, preFetchDateTime < MessageUtils.GetDateTimeFromSecureStorageForKey(SecureStorageKeys.LAST_SENT_NOTIFICATION_UTC_KEY, ""));
@@ -85,6 +104,7 @@ namespace NDB.Covid19.Test.Tests.ExposureNotification
         private void ResetData()
         {
             SystemTime.ResetDateTime();
+            MessageUtils.RemoveAll();
             Permissions.BluetoothEnabled = true;
             Permissions.LocationEnabled = true;
             LocalNotificationsManager.ResetHasBeenCalledMap();
