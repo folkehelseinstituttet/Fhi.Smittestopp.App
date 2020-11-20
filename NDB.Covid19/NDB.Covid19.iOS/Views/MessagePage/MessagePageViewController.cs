a3ffb41aa40cfeeb8e53b8bd80cde44927dfdbba
Jonas Oksvold Frich <jonasf@netcompany.com>
2020-11-20 15:44:18 +0100

    Changed navigation implementation when tapping notification, and cleaned up now unused method.

--- NDB.Covid19/NDB.Covid19.iOS/Views/MessagePage/MessagePageViewController.cs
+++ NDB.Covid19/NDB.Covid19.iOS/Views/MessagePage/MessagePageViewController.cs
@@ -25,15 +25,6 @@ namespace NDB.Covid19.iOS.Views.MessagePage
             return vc;
         }
 
-        public static UINavigationController GetMessagePageControllerInNavigationController()
-        {
-            UIViewController vc = MessagePageViewController.Create();
-            UINavigationController navigationController = new UINavigationController(vc);
-            navigationController.SetNavigationBarHidden(true, false);
-            navigationController.ModalPresentationStyle = UIModalPresentationStyle.FullScreen;
-            return navigationController;
-        }
-
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
