00ee77ffe8700e3ed0358a0a1b525b5cae6173ac
Jonas Frich <jonas.frich@gmail.com>
2020-11-25 20:42:46 +0100

    [48404] Implemented message page iOS (#30)
    * Implemented message page iOS
    
    * Implemented unread label
    
    * Implemented design for last updated label in MessagePage
    
    * Added translations for message header
    
    Co-authored-by: Jonas Oksvold Frich <jonasf@netcompany.com>
    Co-authored-by: Kamil Nować <kano@netcompany.com>
    Co-authored-by: Frederik Skou Nielsen <60694331+Frederik-S-N-dk@users.noreply.github.com>
    Co-authored-by: Pavel Shramau <pavels@netcompany.com>

--- NDB.Covid19/NDB.Covid19.iOS/Views/MessagePage/MessageTableViewSource.cs
+++ NDB.Covid19/NDB.Covid19.iOS/Views/MessagePage/MessageTableViewSource.cs
@@ -7,6 +7,7 @@ using NDB.Covid19.ViewModels;
 using NDB.Covid19.Interfaces;
 using UIKit;
 using Xamarin.Essentials;
+using NDB.Covid19.iOS.Utils;
 
 namespace NDB.Covid19.iOS.Views.MessagePage
 {
@@ -23,14 +24,35 @@ namespace NDB.Covid19.iOS.Views.MessagePage
         public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
         {
             MessagePageCell cell = tableView.DequeueReusableCell(MessagePageCell.Key, indexPath) as MessagePageCell;
-            MessageItemViewModel model = _models[indexPath.Row];
+            MessageItemViewModel model = _models[indexPath.Section];
+            cell.Layer.CornerRadius = 14;
+            cell.Layer.BorderColor = ColorHelper.MESSAGE_BORDER_COLOR.CGColor;
+            cell.Layer.BorderWidth = 1;
+            cell.ClipsToBounds = true;
             cell.Update(model);
             return cell;
         }
 
-        public override nint RowsInSection(UITableView tableview, nint section)
+        public override nint NumberOfSections(UITableView tableView)
         {
             return _models.Count();
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return 1;
+        }
+
+        public override nfloat GetHeightForHeader(UITableView tableView, nint section)
+        {
+            return 20;
+        }
+
+        public override UIView GetViewForHeader(UITableView tableView, nint section)
+        {
+            UIView view = new UIView();
+            view.BackgroundColor = UIColor.Clear;
+            return view;
         }
 
         internal void Update(List<MessageItemViewModel> models)
@@ -40,8 +62,8 @@ namespace NDB.Covid19.iOS.Views.MessagePage
 
         public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
         {
-           ServiceLocator.Current.GetInstance<IBrowser>().OpenAsync(_models[indexPath.Row].MessageLink, BrowserLaunchMode.SystemPreferred);
-           _models[indexPath.Row].IsRead = true;
+           ServiceLocator.Current.GetInstance<IBrowser>().OpenAsync(_models[indexPath.Section].MessageLink, BrowserLaunchMode.SystemPreferred);
+           _models[indexPath.Section].IsRead = true;
             tableView.ReloadData();
         }
     }
