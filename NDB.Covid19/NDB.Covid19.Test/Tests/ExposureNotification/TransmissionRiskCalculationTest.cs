4407dba3b19d5aa825375b45903af26540e6bcfc
Kamil Nować <kano@netcompany.com>
2020-11-26 16:35:48 +0100

    Add logs

--- NDB.Covid19/NDB.Covid19.Test/Tests/ExposureNotification/TransmissionRiskCalculationTest.cs
+++ NDB.Covid19/NDB.Covid19.Test/Tests/ExposureNotification/TransmissionRiskCalculationTest.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NDB.Covid19.Models;
 using Xamarin.ExposureNotifications;
 using Xunit;
+using Xunit.Abstractions;
 using static NDB.Covid19.ExposureNotifications.Helpers.UploadDiagnosisKeysHelper;
 
 namespace NDB.Covid19.Test.Tests.ExposureNotification
 {
     public class TransmissionRiskCalculationTest
     {
+        private readonly ITestOutputHelper output;
+        public TransmissionRiskCalculationTest(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+        
         // 00:00 1 June 2020 UTC
         private readonly DateTimeOffset june1 = DateTimeOffset.FromUnixTimeSeconds(1590969600).ToUniversalTime();
 
@@ -52,7 +60,10 @@ namespace NDB.Covid19.Test.Tests.ExposureNotification
                 CreateAValidListOfTemporaryExposureKeys(processedKeys);
 
             List<ExposureKeyModel> resultKeys = SetTransmissionRiskLevel(validKeys, MiBaDate);
-
+            output.WriteLine("processedKeys {0}", processedKeys.Count());
+            output.WriteLine("validKeys {0}", validKeys.Count());
+            output.WriteLine("resultKeys {0}", resultKeys.Count());
+            
             AssertPositiveDaysTEKS(resultKeys);
 
             IEnumerable<ExposureKeyModel> processedNegativeDifferenceExposureKeys =
@@ -63,6 +74,10 @@ namespace NDB.Covid19.Test.Tests.ExposureNotification
             List<ExposureKeyModel> resultKeysNegativeDifference =
                 SetTransmissionRiskLevel(validNegativeDifferenceExposureKeys, MiBaDate);
 
+            output.WriteLine("processedNegativeDifferenceExposureKeys {0}", processedNegativeDifferenceExposureKeys.Count());
+            output.WriteLine("validNegativeDifferenceExposureKeys {0}", validNegativeDifferenceExposureKeys.Count());
+            output.WriteLine("resultKeysNegativeDifference {0}", resultKeysNegativeDifference.Count());
+            
             AssertNegativeDaysTEKS(resultKeysNegativeDifference);
         }
 
