09e3e04e9ecd9389663989ed536bdff7b0cca7dc
Jonas Frich <jonas.frich@gmail.com>
2020-11-27 14:31:43 +0100

    [50138] iOS: Implemented custom back button with larger tap area and placed it in all relevant storyboards (#66)

---
+++ NDB.Covid19/NDB.Covid19.iOS/Views/CustomSubclasses/CustomBackButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace NDB.Covid19.iOS.Views.CustomSubclasses
+{
+    [Register("CustomBackButton")]
+    public class CustomBackButton : UIButton
+    {
+
+        public nfloat ExtendedArea { get; set; } = 34;
+
+        public CustomBackButton(IntPtr handle) : base(handle)
+        {
+            var imageView = this.ImageView;
+            imageView.UserInteractionEnabled = false;
+        }
+
+        public override bool PointInside(CGPoint point, UIEvent uievent)
+        {
+            var extendedArea = new CGRect
+            {
+                X = this.Bounds.Location.X - ExtendedArea,
+                Y = this.Bounds.Location.Y - ExtendedArea,
+                Width = this.Bounds.Size.Width + ExtendedArea * 2,
+                Height = this.Bounds.Size.Width + ExtendedArea * 2
+            };
+
+            return extendedArea.Contains(point);
+        }
+    }
+}
