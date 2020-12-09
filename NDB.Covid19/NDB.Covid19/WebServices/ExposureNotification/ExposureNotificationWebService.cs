8bddc7dd98f7848e5a1712c6653aef836b5f7331
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-12-09 12:13:29 +0100

    Add Initiator inside the feature toggle
    Including placeholders for reference for future work.

--- NDB.Covid19/NDB.Covid19/WebServices/ExposureNotification/ExposureNotificationWebService.cs
+++ NDB.Covid19/NDB.Covid19/WebServices/ExposureNotification/ExposureNotificationWebService.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using AnonymousTokens.Client.Protocol;
+
+using NDB.Covid19.AnonymousTokens;
 using NDB.Covid19.Configuration;
 using NDB.Covid19.Models;
+using NDB.Covid19.OAuth2;
 using NDB.Covid19.PersistedData;
 using NDB.Covid19.Utils;
 using NDB.Covid19.ViewModels;
 using NDB.Covid19.WebServices.ErrorHandlers;
 using NDB.Covid19.WebServices.Helpers;
+
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto.EC;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NDB.Covid19.WebServices.ExposureNotification
 {
@@ -27,25 +35,47 @@ namespace NDB.Covid19.WebServices.ExposureNotification
 
         public async Task<bool> PostSelvExposureKeys(SelfDiagnosisSubmissionDTO selfDiagnosisSubmissionDTO, IEnumerable<ExposureKeyModel> temporaryExposureKeys, BaseWebService service)
         {
-            ApiResponse response = await service.Post(selfDiagnosisSubmissionDTO, Conf.URL_PUT_UPLOAD_DIAGNOSIS_KEYS);
-            
-            // HandleErrorsSilently happens even if IsSuccessfull is true other places in the code, but here
-            // we have an if-else to avoid having to create the redacted key list if we don't have to
-            if (!response.IsSuccessfull)
+            if (AuthenticationState.PersonalData?.AnonymousTokensEnabled == true)
             {
-                string redactedKeysJson = RedactedTekListHelper.CreateRedactedTekList(temporaryExposureKeys);
-                HandleErrorsSilently(response, new PostExposureKeysErrorHandler(redactedKeysJson));
+                var ecParameters = CustomNamedCurves.GetByOid(X9ObjectIdentifiers.Prime256v1);
+                var publicKeyStore = new PublicKeyStore();
+                var publicKey = await publicKeyStore.GetAsync();
+
+                var initiator = new Initiator();
+                var init = initiator.Initiate(ecParameters.Curve);
+                var t = init.t;
+                var r = init.r;
+                var P = init.P;
+
+                // call /api/anonymoustokens on Verification with P as payload to generate token. Response will contain Q, proofC, proofZ.
+                // var W = initiator.RandomiseToken(ecParameters, publicKey, P, Q, proofC, proofZ, r);
+                // call /xx/xx on Backend with t and W as payload to verify the token. Response will be true/false.
+
+                return await Task.FromResult(true);
             }
             else
             {
-                HandleErrorsSilently(response);
-            }
-            
-            ENDeveloperToolsViewModel.UpdatePushKeysInfo(response, selfDiagnosisSubmissionDTO, JsonSerializerSettings);
+                ApiResponse response = await service.Post(selfDiagnosisSubmissionDTO, Conf.URL_PUT_UPLOAD_DIAGNOSIS_KEYS);
 
-            return response.IsSuccessfull;
+                // HandleErrorsSilently happens even if IsSuccessfull is true other places in the code, but here
+                // we have an if-else to avoid having to create the redacted key list if we don't have to
+                if (!response.IsSuccessfull)
+                {
+                    string redactedKeysJson = RedactedTekListHelper.CreateRedactedTekList(temporaryExposureKeys);
+                    HandleErrorsSilently(response, new PostExposureKeysErrorHandler(redactedKeysJson));
+                }
+                else
+                {
+                    HandleErrorsSilently(response);
+                }
+
+                ENDeveloperToolsViewModel.UpdatePushKeysInfo(response, selfDiagnosisSubmissionDTO, JsonSerializerSettings);
+
+                return response.IsSuccessfull;
+            }
+
         }
-        
+
         public async Task<Xamarin.ExposureNotifications.Configuration> GetExposureConfiguration()
         {
             ApiResponse<AttenuationBucketsConfigurationDTO> response = await Get<AttenuationBucketsConfigurationDTO>(Conf.URL_GET_EXPOSURE_CONFIGURATION);
