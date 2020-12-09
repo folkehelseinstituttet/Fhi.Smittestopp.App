40df77d07438b3a29a6ae8e03542b36c48e76523
Henrik W. Moe <henrik.walker.moe@bekk.no>
2020-12-09 12:11:08 +0100

    Add PublicKeyStore for retrieving the public key

---
+++ NDB.Covid19/NDB.Covid19/AnonymousTokens/PublicKeyStore.cs
@@ -0,0 +1,23 @@
+﻿using AnonymousTokens.Core.Services;
+
+using NDB.Covid19.Configuration;
+
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace NDB.Covid19.AnonymousTokens
+{
+    public class PublicKeyStore : IPublicKeyStore
+    {
+        public Task<ECPublicKeyParameters> GetAsync()
+        {
+            var certificate = new X509Certificate2(OAuthConf.OAUTH2_VERIFY_TOKEN_PUBLIC_KEY);
+
+            var convertedCertificate = DotNetUtilities.FromX509Certificate(certificate);
+            return Task.FromResult((ECPublicKeyParameters)convertedCertificate.GetPublicKey());
+        }
+    }
+}
