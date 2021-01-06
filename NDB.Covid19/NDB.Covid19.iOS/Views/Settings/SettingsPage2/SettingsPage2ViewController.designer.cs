87974df3cdb38b89acc922421226404863e53e62
Jonas Oksvold Frich <jonasf@netcompany.com>
2021-01-06 10:22:25 +0100

    - Included changes from previous PR splitting up texts in how it works page - Added new section to how it works page

--- NDB.Covid19/NDB.Covid19.iOS/Views/Settings/SettingsPage2/SettingsPage2ViewController.designer.cs
+++ NDB.Covid19/NDB.Covid19.iOS/Views/Settings/SettingsPage2/SettingsPage2ViewController.designer.cs
@@ -1,51 +1,159 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace NDB.Covid19.iOS.Views.Settings.SettingsPage2
 {
-    [Register ("SettingsPage2ViewController")]
-    partial class SettingsPage2ViewController
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton BackButton { get; set; }
+	[Register ("SettingsPage2ViewController")]
+	partial class SettingsPage2ViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton BackButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextView ContentText { get; set; }
+		[Outlet]
+		UIKit.UILabel Header1Label { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        NDB.Covid19.iOS.Views.CustomSubclasses.SetttingsPageTitleLabel HeaderLabel { get; set; }
+		[Outlet]
+		UIKit.UILabel Header2Label { get; set; }
 
-        [Action ("BackButton_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void BackButton_TouchUpInside (UIKit.UIButton sender);
+		[Outlet]
+		UIKit.UILabel Header3Label { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (BackButton != null) {
-                BackButton.Dispose ();
-                BackButton = null;
-            }
+		[Outlet]
+		UIKit.UILabel Header4Label { get; set; }
 
-            if (ContentText != null) {
-                ContentText.Dispose ();
-                ContentText = null;
-            }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		NDB.Covid19.iOS.Views.CustomSubclasses.SetttingsPageTitleLabel HeaderLabel { get; set; }
 
-            if (HeaderLabel != null) {
-                HeaderLabel.Dispose ();
-                HeaderLabel = null;
-            }
-        }
-    }
+		[Outlet]
+		UIKit.UILabel IntroLabel { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph1Label1 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph1Label2 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph2Label1 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph2Label2 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph2Label3 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph2Label4 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph3Label1 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph4Label1 { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Paragraph4Label2 { get; set; }
+
+		[Outlet]
+		UIKit.UITextView Paragraph4Label3 { get; set; }
+
+		[Action ("BackButton_TouchUpInside:")]
+		partial void BackButton_TouchUpInside (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (BackButton != null) {
+				BackButton.Dispose ();
+				BackButton = null;
+			}
+
+			if (Header1Label != null) {
+				Header1Label.Dispose ();
+				Header1Label = null;
+			}
+
+			if (Header2Label != null) {
+				Header2Label.Dispose ();
+				Header2Label = null;
+			}
+
+			if (Header3Label != null) {
+				Header3Label.Dispose ();
+				Header3Label = null;
+			}
+
+			if (Header4Label != null) {
+				Header4Label.Dispose ();
+				Header4Label = null;
+			}
+
+			if (HeaderLabel != null) {
+				HeaderLabel.Dispose ();
+				HeaderLabel = null;
+			}
+
+			if (IntroLabel != null) {
+				IntroLabel.Dispose ();
+				IntroLabel = null;
+			}
+
+			if (Paragraph1Label1 != null) {
+				Paragraph1Label1.Dispose ();
+				Paragraph1Label1 = null;
+			}
+
+			if (Paragraph1Label2 != null) {
+				Paragraph1Label2.Dispose ();
+				Paragraph1Label2 = null;
+			}
+
+			if (Paragraph2Label1 != null) {
+				Paragraph2Label1.Dispose ();
+				Paragraph2Label1 = null;
+			}
+
+			if (Paragraph2Label2 != null) {
+				Paragraph2Label2.Dispose ();
+				Paragraph2Label2 = null;
+			}
+
+			if (Paragraph2Label3 != null) {
+				Paragraph2Label3.Dispose ();
+				Paragraph2Label3 = null;
+			}
+
+			if (Paragraph2Label4 != null) {
+				Paragraph2Label4.Dispose ();
+				Paragraph2Label4 = null;
+			}
+
+			if (Paragraph3Label1 != null) {
+				Paragraph3Label1.Dispose ();
+				Paragraph3Label1 = null;
+			}
+
+			if (Paragraph4Label1 != null) {
+				Paragraph4Label1.Dispose ();
+				Paragraph4Label1 = null;
+			}
+
+			if (Paragraph4Label2 != null) {
+				Paragraph4Label2.Dispose ();
+				Paragraph4Label2 = null;
+			}
+
+			if (Paragraph4Label3 != null) {
+				Paragraph4Label3.Dispose ();
+				Paragraph4Label3 = null;
+			}
+		}
+	}
 }
