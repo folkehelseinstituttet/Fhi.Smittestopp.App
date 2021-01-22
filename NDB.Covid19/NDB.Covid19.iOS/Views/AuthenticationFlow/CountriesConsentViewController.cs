abe989a842e3b6aa078070952408f758f2585761
Frederik-S-N-dk <fsn@netcompany.com>
2021-01-22 17:06:50 +0100

    iOS - Updated consent 2+3 layout

--- NDB.Covid19/NDB.Covid19.iOS/Views/AuthenticationFlow/CountriesConsentViewController.cs
+++ NDB.Covid19/NDB.Covid19.iOS/Views/AuthenticationFlow/CountriesConsentViewController.cs
@@ -28,10 +28,10 @@ namespace NDB.Covid19.iOS.Views.AuthenticationFlow
             
             CloseBtn.AccessibilityLabel = CLOSE_BUTTON_ACCESSIBILITY_LABEL;
             HeaderLabel.SetAttributedText(HEADER_TEXT);
-            DescriptionLabel.SetAttributedText(CONSENT3_BODYTEXT_1);
-            LookUp_Text.SetAttributedText(CONSENT3_BODYTEXT_2);
-            Notification_Text.SetAttributedText(CONSENT3_EU_CONSENT_BUTTON_BODYTEXT);
-            Consent_BeAware_Text.SetAttributedText(CONSENT3_ONLY_NORWAY_CONSENT_BUTTON_BODYTEXT);
+            BodyText1.SetAttributedText(CONSENT3_BODYTEXT_1);
+            BodyText2.SetAttributedText(CONSENT3_BODYTEXT_2);
+            ConsentEU_Explanation.SetAttributedText(CONSENT3_EU_CONSENT_BUTTON_BODYTEXT);
+            Consent_onlyNorway_Explanation.SetAttributedText(CONSENT3_ONLY_NORWAY_CONSENT_BUTTON_BODYTEXT);
 
             StyleUtil.InitButtonStyling(NextButtonWithEUConsent, EU_CONSENT_NEXT_EU_CONSENT_BUTTON_TEXT);
             StyleUtil.InitButtonStyling(NextButtonOnlyNorwayConsent, EU_CONSENT_NEXT_ONLY_NORWAY_CONSENT_BUTTON_TEXT);
@@ -43,13 +43,10 @@ namespace NDB.Covid19.iOS.Views.AuthenticationFlow
         public void SetupStyling()
         {
             HeaderLabel.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            DescriptionLabel.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            LookUp_Header.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            LookUp_Text.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            Notification_Header.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            Notification_Text.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            Consent_BeAware_Text.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
-            Consent_Explanation_Text.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
+            BodyText1.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
+            BodyText2.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
+            ConsentEU_Explanation.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
+            Consent_onlyNorway_Explanation.TextColor = ColorHelper.TEXT_COLOR_ON_BACKGROUND;
         }
 
         void GoToLoadingPage()
