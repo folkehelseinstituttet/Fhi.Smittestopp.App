eb65be3a6a7a8fd7b8d4679f857d6976c477b614
Jonas Frich <jonas.frich@gmail.com>
2021-01-27 11:02:14 +0100

    [Feature/52349] Implemented link button to accessibility statement in about page  (#273)
    * Added strings to locales and viewmodel
    
    * - Fixed spelling error
    - Implemented button on Android
    
    * - Implemented button on iOS
    - Extracted resize button to StyleUtil
    
    * Added actual link
    
    Co-authored-by: Jonas Oksvold Frich <jonasf@netcompany.com>

--- NDB.Covid19/NDB.Covid19.iOS/Views/ConsentView/ConsentViewController.cs
+++ NDB.Covid19/NDB.Covid19.iOS/Views/ConsentView/ConsentViewController.cs
@@ -169,11 +169,7 @@ namespace NDB.Covid19.iOS.Views.ConsentView
         void InitPrivacyPolicyButton()
         {
             InitButtonSecondaryStyling(PrivacyPolicy, ConsentViewModel.CONSENT_SEVEN_BUTTON_TEXT);
-
-            double height = PrivacyPolicy.TitleLabel.Frame.Height + PrivacyPolicy.Font.PointSize * 1.5;
-            PrivacyPolicy.Frame = new CGRect(0, 0, 0, height); // The frame should not be needed here, but it is since the cornerRadius in StyleUtil is set only once, not dynamically updated on redraw.
-            PrivacyPolicy.TranslatesAutoresizingMaskIntoConstraints = false;
-            PrivacyPolicy.HeightAnchor.ConstraintEqualTo((int) height).Active = true;
+            ResizeSecondaryButton(PrivacyPolicy);
         }
 
         partial void PrivacyPolicy_TouchUpInside(UIButton sender)
