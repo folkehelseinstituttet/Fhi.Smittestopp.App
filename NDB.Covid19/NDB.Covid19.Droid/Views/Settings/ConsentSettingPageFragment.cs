d62ce4d1149e190ca441dd5b87d6b0fcb6706dfb
Mikkel Hansen <78138025+MIKKH@users.noreply.github.com>
2021-01-28 14:10:29 +0100

    [Issue 89][Android] Contact information about "personvernombud" in consent 1 + adjusted "personvernerklæring"-button (#299)
    * Divided 'CONSENT_FIVE_PARAGRAPH' into three sections.
    
    * Split consent paragraph 5 into three sections and added new text about Data Protection Officer (personvernombud).
    
    * Split 'consent paragraph 5' into three sections.
    
    * Updated textxs about "Personvernombud"
    
    * Replaced button for privacy policy ("personvernerklæringen") with a text link.
    
    * Changed naming of locales keys:
    - 'CONSENT_SEVEN_BUTTON_TEXT' -> 'CONSENT_SEVEN_LINK_TEXT'
    - 'CONSENT_SEVEN_BUTTON_URL' -> 'CONSENT_SEVEN_LINK_URL'
    
    * Changed naming of locales keys:
    - 'CONSENT_SEVEN_BUTTON_TEXT' -> 'CONSENT_SEVEN_LINK_TEXT'
    - 'CONSENT_SEVEN_BUTTON_URL' -> 'CONSENT_SEVEN_LINK_URL'
    
    * Changed naming of resource ID's to English.
    
    * Checkout iOS project from 'dev-r2_x' branch.
    
    * Checkout iOS project from 'dev-r2_x' branch.
    
    * Checkout iOS project file from 'dev-r2_x'
    
    Co-authored-by: Jonas Frich <jonas.frich@gmail.com>

--- NDB.Covid19/NDB.Covid19.Droid/Views/Settings/ConsentSettingPageFragment.cs
+++ NDB.Covid19/NDB.Covid19.Droid/Views/Settings/ConsentSettingPageFragment.cs
@@ -21,7 +21,10 @@ namespace NDB.Covid19.Droid.Views.Settings
         TextView _howitworksText2;
         TextView _samtykkeText1;
         TextView _samtykkeText2;
-        TextView _behandlingafpersonoplysningerText;
+        TextView _processingofpersonaldataText1;
+        TextView _processingofpersonaldataText2;
+        TextView _processingofpersonaldataText3;
+        TextView _policyLinkText;
         TextView _samtykkebottomHeader;
         TextView _samtykkebottomText;
 
@@ -55,8 +58,17 @@ namespace NDB.Covid19.Droid.Views.Settings
             _samtykkeText2.Text = ConsentViewModel.CONSENT_THREE_PARAGRAPH_SECTION_TWO;
 
             //BEHANDLING AF PERSONOPLYSNINGWER
-            _behandlingafpersonoplysningerText = view.FindViewById<TextView>(Resource.Id.consent1_behandlingafpersonopl_text);
-            _behandlingafpersonoplysningerText.Text = ConsentViewModel.CONSENT_FIVE_PARAGRAPH;
+            _processingofpersonaldataText1 = view.FindViewById<TextView>(Resource.Id.consent1_processingofpersonaldata_text_section1);
+            _processingofpersonaldataText1.Text = ConsentViewModel.CONSENT_FIVE_PARAGRAPH_SECTION_ONE;
+            _processingofpersonaldataText2 = view.FindViewById<TextView>(Resource.Id.consent1_processingofpersonaldata_text_section2);
+            _processingofpersonaldataText2.Text = ConsentViewModel.CONSENT_FIVE_PARAGRAPH_SECTION_TWO;
+            _processingofpersonaldataText3 = view.FindViewById<TextView>(Resource.Id.consent1_processingofpersonaldata_text_section3);
+            _processingofpersonaldataText3.Text = ConsentViewModel.CONSENT_FIVE_PARAGRAPH_SECTION_THREE;
+
+            //PERSONVERNERKLÆRINGEN LINK
+            _policyLinkText = view.FindViewById<TextView>(Resource.Id.consent_paragraph_policy_link);
+            _policyLinkText.TextFormatted = HtmlCompat.FromHtml($"<a href=\"{ConsentViewModel.CONSENT_SEVEN_LINK_URL}\">{ConsentViewModel.CONSENT_SEVEN_LINK_TEXT}</a>", HtmlCompat.FromHtmlModeLegacy);
+            _policyLinkText.MovementMethod = new Android.Text.Method.LinkMovementMethod();
 
             //SAMTYKKE, BOTTOM
             _samtykkebottomHeader = view.FindViewById<TextView>(Resource.Id.consent1_samtykkebottom_header);
@@ -64,10 +76,6 @@ namespace NDB.Covid19.Droid.Views.Settings
             _samtykkebottomText = view.FindViewById<TextView>(Resource.Id.consent1_samtykkebottom_text);
             _samtykkebottomText.Text = ConsentViewModel.CONSENT_SIX_PARAGRAPH;
 
-
-            Button policyLinkBtn = view.FindViewById<Button>(Resource.Id.consent_paragraph_policy_btn);
-            policyLinkBtn.Text = ConsentViewModel.CONSENT_SEVEN_BUTTON_TEXT;
-            policyLinkBtn.Click += PolicyLinkBtn_Click;
 
             // CONTENT DESCRIPTIONS OF HEADER
             _aboutHeader.ContentDescription = ConsentViewModel.CONSENT_ONE_TITLE.ToLower();
