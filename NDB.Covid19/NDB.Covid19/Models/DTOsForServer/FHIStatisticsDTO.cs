5dd99a633664ba9a18f8fba908c3c910804946c7
Michael-Olesen <mbol@netcompany.com>
2021-02-16 16:18:47 +0100

    Added additional json keys and connected them to the UI elements

--- NDB.Covid19/NDB.Covid19/Models/DTOsForServer/FHIStatisticsDTO.cs
+++ NDB.Covid19/NDB.Covid19/Models/DTOsForServer/FHIStatisticsDTO.cs
@@ -12,5 +12,11 @@ namespace NDB.Covid19.Models.DTOsForServer
         public int TestsConductedToday { get; set; }
         public int TestsConductedTotal { get; set; }
         public int PatientsAdmittedToday { get; set; }
+        public int PatientsIntensiveCare { get; set; }
+        public double ReproductionsNumber { get; set; }
+        public int VaccinationsDoseOneToday { get; set; }
+        public int VaccinationsDoseTwoToday { get; set; }
+        public int VaccinationsDoseOneTotal { get; set; }
+        public int VaccinationsDoseTwoTotal { get; set; }
     }
 }
