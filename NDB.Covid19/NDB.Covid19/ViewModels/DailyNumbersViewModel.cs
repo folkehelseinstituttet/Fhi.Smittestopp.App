bed78c70aee9d568ad4163ef79d12288a56cbee3
Mikkel Hansen <mikkh@netcompany.com>
2021-02-23 12:07:55 +0100

    Added missing keys to 'DailyNumbersViewModel'

--- NDB.Covid19/NDB.Covid19/ViewModels/DailyNumbersViewModel.cs
+++ NDB.Covid19/NDB.Covid19/ViewModels/DailyNumbersViewModel.cs
@@ -14,6 +14,9 @@ namespace NDB.Covid19.ViewModels
         public static string DAILY_NUMBERS_HEADER => "DAILY_NUMBERS_HEADER".Translate();
         public static string DAILY_NUMBERS_TITLE_ONE => "DAILY_NUMBERS_TITLE_ONE".Translate();
         public static string DAILY_NUMBERS_SUBHEADER => "DAILY_NUMBERS_SUBHEADER".Translate();
+        public static string DAILY_NUMBERS_TITLE_TWO => "DAILY_NUMBERS_TITLE_TWO".Translate();
+        public static string DAILY_NUMBERS_SUBTEXT_TWO => "DAILY_NUMBERS_SUBTEXT_TWO".Translate();
+        public static string DAILY_NUMBERS_TITLE_THREE => "DAILY_NUMBERS_TITLE_THREE".Translate();
         public static string KEY_FEATURE_ONE_UPDATE_NEW => "KEY_FEATURE_ONE_UPDATE_NEW".Translate();
         public static string KEY_FEATURE_ONE_UPDATE_ALL => "KEY_FEATURE_ONE_UPDATE_ALL".Translate();
         public static string KEY_FEATURE_ONE_LABEL => "KEY_FEATURE_ONE_LABEL".Translate();
@@ -44,6 +47,16 @@ namespace NDB.Covid19.ViewModels
         public static string KEY_FEATURE_TEN_UPDATE_ALL => "KEY_FEATURE_TEN_UPDATE_ALL".Translate();
         public static string DAILY_NUMBERS_TITLE_THREE => "DAILY_NUMBERS_TITLE_THREE".Translate();
         public static string DAILY_NUMBERS_SUBSUBHEADER => "DAILY_NUMBERS_SUBSUBHEADER".Translate();
+        public static string KEY_FEAUTRE_SEVEN_LABEL => "KEY_FEATURE_SEVEL_LABEL".Translate();
+        public static string KEY_FEATURE_SEVEN_UPDATE_ALL => "KEY_FEAUTE_SEVEN_UPDATE_ALL".Translate();
+        public static string KEY_FEATURE_EIGHT_LABEL => "KEY_FEATURE_EIGHT_LABEL".Translate();
+        public static string KEY_FEATURE_EIGHT_UPDATE_ALL => "KEY_FEATURE_EIGHT_UPDATE_ALL".Translate();
+        public static string KEY_FEATURE_NINE_LABEL => "KEY_FEATURE_NINE_LABEL".Translate();
+        public static string KEY_FEATURE_NINE_UPDATE_NEW => "KEY_FEATURE_NINE_UPDATE_NEW".Translate();
+        public static string KEY_FEATURE_NINE_UPDATE_ALL => "KEY_FEATURE_NINE_UPDATE_ALL".Translate();
+        public static string KEY_FEATURE_TEN_LABEL => "KEY_FEATURE_TEN_LABEL".Translate();
+        public static string KEY_FEATURE_TEN_UPDATE_NEW => "KEY_FEATURE_TEN_UPDATE_NEW".Translate();
+        public static string KEY_FEATURE_TEN_UPDATE_ALL => "KEY_FEATURE_TEN_UPDATE_ALL".Translate();
         private static readonly DailyNumbersWebService WebService;
 
         static DailyNumbersViewModel()
