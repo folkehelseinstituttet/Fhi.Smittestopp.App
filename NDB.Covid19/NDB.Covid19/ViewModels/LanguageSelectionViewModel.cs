1298a6f2c15e278e612ffc4f76f05a6c5208eb1c
Jonas Oksvold Frich <jonasf@netcompany.com>
2021-02-25 14:33:25 +0100

    iOS: Cleaned up previous code and implemented Lithuanian in language selection and settings

--- NDB.Covid19/NDB.Covid19/ViewModels/LanguageSelectionViewModel.cs
+++ NDB.Covid19/NDB.Covid19/ViewModels/LanguageSelectionViewModel.cs
@@ -8,6 +8,7 @@ namespace NDB.Covid19.ViewModels
         public static string LANGUAGE_SELECTION_NN => "LANGUAGE_SELECTION_NN".Translate();
         public static string LANGUAGE_SELECTION_EN => "LANGUAGE_SELECTION_EN".Translate();
         public static string LANGUAGE_SELECTION_AR => "LANGUAGE_SELECTION_AR".Translate();
+        public static string LANGUAGE_SELECTION_LT => "LANGUAGE_SELECTION_LT".Translate();
         public static string LANGUAGE_SELECTION_PL => "LANGUAGE_SELECTION_PL".Translate();
         public static string LANGUAGE_SELECTION_SO => "LANGUAGE_SELECTION_SO".Translate();
         public static string LANGUAGE_SELECTION_TI => "LANGUAGE_SELECTION_TI".Translate();
