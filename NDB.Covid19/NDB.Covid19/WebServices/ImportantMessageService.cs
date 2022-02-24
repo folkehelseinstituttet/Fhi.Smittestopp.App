54e9a362371f2e350bd2c66419ce8231412ad72c
Christopher Bauge <bauge.christopher@gmail.com>
2022-02-24 09:32:41 +0100

    [66263] - Show latest fetched message response in dev tools (#616)
    * Add last fetched message for dev-tools
    
    * Use helper class to get current datetime
    
    Co-authored-by: Pavel Shramau <pavels@netcompany.com>

--- NDB.Covid19/NDB.Covid19/WebServices/ImportantMessageService.cs
+++ NDB.Covid19/NDB.Covid19/WebServices/ImportantMessageService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CommonServiceLocator;
 using NDB.Covid19.Configuration;
+using NDB.Covid19.ExposureNotifications.Helpers;
 using NDB.Covid19.Models;
 using NDB.Covid19.PersistedData;
+using NDB.Covid19.Utils.DeveloperTools;
 
 namespace NDB.Covid19.WebServices
 {
@@ -12,6 +15,8 @@ namespace NDB.Covid19.WebServices
         {
             ApiResponse<ImportantMessage> response = await Get<ImportantMessage>($"{Conf.URL_GET_IMPORTANT_MESSAGE}?lang={LocalPreferencesHelper.GetAppLanguage()}");
             HandleErrorsSilently(response);
+
+            ServiceLocator.Current.GetInstance<IDeveloperToolsService>().SaveLastFetchedImportantMessage(response, SystemTime.Now());
 
             return response?.Data;
         }
