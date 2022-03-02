203bc18dd52b71aa10a7d183d43bac96b095535e
Christopher Bauge <bauge.christopher@gmail.com>
2022-03-02 10:38:44 +0100

    [66475][iOS] - Bugfix error-button resizing (#620)

---
+++ NDB.Covid19/NDB.Covid19.iOS/Views/CustomSubclasses/CustomResizableButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace NDB.Covid19.iOS.Views.CustomSubclasses
+{
+    [Register("CustomResizableButton")]
+    public class CustomResizableButton : UIButton
+    {
+        public CustomResizableButton(IntPtr handle) : base(handle) { }
+
+        public override CGSize IntrinsicContentSize
+        {
+            get
+            {
+                CGSize labelSize = TitleLabel.IntrinsicContentSize;
+                CGSize buttonSize = new CGSize(labelSize.Width + TitleEdgeInsets.Left + TitleEdgeInsets.Right,
+                    labelSize.Height + TitleEdgeInsets.Top + TitleEdgeInsets.Bottom + 10);
+
+                return buttonSize;
+            }
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            TitleLabel.PreferredMaxLayoutWidth = TitleLabel.Frame.Size.Width;
+        }
+    }
+}
